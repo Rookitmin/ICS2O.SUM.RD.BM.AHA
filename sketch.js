42f2ca4d3bf1c0847ddf61f28633c68b1a7f0a77
Rookitmin <42980035+Rookitmin@users.noreply.github.com>
2018-11-02 13:59:38 -0400

    Another Update.
    Added a end Game Scene
    Fixed The Credit Scene.
    Current Scenes are:
     - Main Menu
     - Settings Page
     - Difficulty page
     - keybind Page 1
     - keybind Page 2

--- sketch.js
+++ sketch.js
@@ -6,9 +6,11 @@ function setup() {
 // an in-game variable, controlling 
 // where you are looking on the map.
 var scroll = [0, 0];
+var Score = 100;
 // controlls what you see
-var scene = 1;
+var scene = 8;
 var inGame = false;
+var GameMode = 0;
 // Changes the difficulty of the CPUs and the 
 // color of the buttons
 var difficulty = 1;
@@ -16,7 +18,6 @@ var difficulty = 1;
 var changeKey = 0;
 var P1 = {Up: 32, Down: 40, Left: 37, Right: 39, shoot: 77};
 var P2 = {Up: 87, Down: 83, Left: 65, Right: 68, shoot: 82};
-
 // creates genaralised button variables
 function button (x, y, width, height, scene, text)  {
   this.x = x;
@@ -95,13 +96,19 @@ button.prototype.hover = function (lll) {
     }
     this.hovering = false;
   }
-
 }
-
+var credits = "Coded By: Rookitmin, Printear, Winnie And The Guy Next Door." + 
+					 " Also I\'d Like to thank Aadsta, Sheepdude and all of my other" + 
+						" sponsers. This Assingment was coded in Approxamitly ... 10 Hours?" + 
+						" has it really been that long? ... I guess so. huh. well... here are " + 
+						"the real credits: Rookitmin, Ali596087, and Minirals in collaboration " +
+						"With the grade three's ... Hockey Dude, and Ringette Girl.";
+var creditScroll = 400;
 var buttonMenu1 = new button (10, 10, 75, 25, 2, "Main Menu");
 var buttonMenu2 = new button (10, 10, 75, 25, 3, "Main Menu");
 var buttonMenu3 = new button (10, 10, 75, 25, 5, "Main Menu");
 var buttonMenu4 = new button (10, 10, 75, 25, 6, "Main Menu");
+var buttonMenu5 = new button (315, 365, 75, 25, 7, "Main Menu");
 var buttonStart1 = new button (10, 50, 120, 40, 1, "Start");
 var buttonSettings1 = new button (10, 100, 120, 40, 1, "Settings");
 var buttonDifficulty1 = new button (20, 50, 75, 30, 2, "Difficulty");
@@ -116,6 +123,7 @@ var Back4 = new button (325, 10, 50, 20, 6, "back");
 var keybind = new button (20, 100, 75, 30, 2, "Controls");
 var next1 = new button (325, 355, 50, 20, 5, "Next");
 var prev1 = new button (10, 355, 50, 20, 6, "Prev");
+var credits1 = new button (10, 150, 120, 40, 1, "Credits");
 var P1Up = new button (80, 45, 80, 45, 5, 
 											"bind Player 1 Up key");
 var P1Down = new button (80, 135, 80, 45, 5, 
@@ -126,7 +134,6 @@ var P1Right = new button (80, 315, 80, 45, 5,
 											"bind Player 1 Right key");
 var P1Shoot = new button (80, 45, 80, 45, 6, 
 											"bind Player 1 Shoot key");
-
 var P2Up = new button (240, 45, 80, 45, 5, 
 											"bind Player 2 Up key");
 var P2Down = new button (240, 135, 80, 45, 5, 
@@ -137,7 +144,6 @@ var P2Right = new button (240, 315, 80, 45, 5,
 											"bind Player 2 RIght key");
 var P2Shoot = new button (240, 45, 80, 45, 6, 
 											"bind Player 2 Shoot key");
-
 var P1UDLRS = function () {
 	P1Up.hover();
 	P1Down.hover();
@@ -168,54 +174,59 @@ function draw() {
 		P1UDLRS();
 		textSize(12);
 		P2UDLRS();
-		if (changeKey === 1 & keyIsPressed) {
-			P1.Up = keyCode;
-			changeKey = 0;
-		}
-		if (changeKey === 2 & keyIsPressed) {
-			P1.Down = keyCode;
-			changeKey = 0;
-		}
-		if (changeKey === 3 & keyIsPressed) {
-			P1.Left = keyCode;
-			changeKey = 0;
-		}
-		if (changeKey === 4 & keyIsPressed) {
-			P1.Right = keyCode;
-			changeKey = 0;
-		}
-		if (changeKey === 5 & keyIsPressed) {
-			P2.Up = keyCode;
-			changeKey = 0;
-		}
-		if (changeKey === 6 & keyIsPressed) {
-			P2.Down = keyCode;
-			changeKey = 0;
-		}
-		if (changeKey === 7 & keyIsPressed) {
-			P2.Left = keyCode;
-			changeKey = 0;
-		}
-		if (changeKey === 8 & keyIsPressed) {
-			P2.Right = keyCode;
-			changeKey = 0;
-		}
-		if (changeKey === 9 & keyIsPressed) {
-			P1.shoot = keyCode;
-			changeKey = 0;
-		}
-		if (changeKey === 10 & keyIsPressed) {
-			P2.shoot = keyCode;
-			changeKey = 0;
+		// just to make scrolling t o where I want to go Easier.
+		if (true) {
+			if (changeKey === 1 & keyIsPressed) {
+				P1.Up = keyCode;
+				changeKey = 0;
+			}
+			if (changeKey === 2 & keyIsPressed) {
+				P1.Down = keyCode;
+				changeKey = 0;
+			}
+			if (changeKey === 3 & keyIsPressed) {
+				P1.Left = keyCode;
+				changeKey = 0;
+			}
+			if (changeKey === 4 & keyIsPressed) {
+				P1.Right = keyCode;
+				changeKey = 0;
+			}
+			if (changeKey === 5 & keyIsPressed) {
+				P2.Up = keyCode;
+				changeKey = 0;
+			}
+			if (changeKey === 6 & keyIsPressed) {
+				P2.Down = keyCode;
+				changeKey = 0;
+			}
+			if (changeKey === 7 & keyIsPressed) {
+				P2.Left = keyCode;
+				changeKey = 0;
+			}
+			if (changeKey === 8 & keyIsPressed) {
+				P2.Right = keyCode;
+				changeKey = 0;
+			}	
+			if (changeKey === 9 & keyIsPressed) {
+				P1.shoot = keyCode;
+				changeKey = 0;
+			}
+			if (changeKey === 10 & keyIsPressed) {
+				P2.shoot = keyCode;
+				changeKey = 0;
+			}
 		}
     textSize(12);
     buttonMenu1.hover();
     buttonMenu2.hover();
+		buttonMenu5.hover();
 		Back1.hover();
 		Back2.hover();
     textSize(30);
     buttonStart1.hover();
     buttonSettings1.hover();
+		credits1.hover();
     textSize(16);
     buttonDifficulty1.hover();
     buttonDifficulty2.hover();
@@ -223,7 +234,41 @@ function draw() {
 		HardCpu.hover(1);
 		MediumCpu.hover(2);
 		EasyCpu.hover(3);
-
+		if (scene === 7) {
+			background(155, 0, 155);
+			fill(0, 0, 0);
+			text(credits, 40, 0 + creditScroll, 320, 320);
+			textSize(30);
+			if (creditScroll > 0) {
+				text("AND YOU! Thanks For Playing!", 40, 200 + creditScroll, 320, 120);
+			} else if (creditScroll > -50) {
+				text("AND YOU! Thanks For Playing!", 40, 200 + creditScroll / 2, 320, 120);
+			} else {
+				text("AND YOU! Thanks For Playing!", 40, 175, 320, 120);
+			}
+			creditScroll -= 1;
+			if (creditScroll < -400) {
+				scene = 1;
+				creditScroll = 400;
+			}
+		}
+		if (scene === 8) {
+			textSize(35);
+			text("Game Over", 100, 100, 200, 200);
+			textSize(30);
+			if (GameMode === 1 || GameMode === 2) {
+				text("Your Score Is:", 100, 150, 200, 150);
+				text(Score + " points.", 100, 200, 200, 100);
+			}
+			if (GameMode === 3) {
+				text("Your Score Is:", 100, 150, 200, 150);
+				text(Score + " Goals.", 100, 200, 200, 100);
+			}
+			if (GameMode === 4) {
+				text("Your'e in:", 100, 150, 200, 150);
+				text(Score + "th place.", 100, 200, 200, 100);
+			}
+		}
   }
 }
 function mousePressed () {
@@ -231,7 +276,7 @@ function mousePressed () {
   	if (buttonMenu1.hovering || buttonMenu2.hovering) {
 			scene = 1;
 		}
-		if (buttonMenu3.hovering || buttonMenu4.hovering) {    
+		if (buttonMenu3.hovering || buttonMenu4.hovering || buttonMenu5.hovering) {    
 			scene = 1;
   	}
     if (buttonSettings1.hovering) {
@@ -299,6 +344,9 @@ function mousePressed () {
 		}
 		if (prev1.hovering) {
 			scene = 5;
+		}
+		if (credits1.hovering) {
+			scene = 7;
 		}
   }
 }
