e9c0598e16706f67d55751f4a5f456130cb6fff8
Rookitmin <42980035+Rookitmin@users.noreply.github.com>
2018-11-03 10:21:34 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ function setup() {
 // where you are looking on the map.
 var scroll = [0, 0];
 var Score = 100;
+var Players = 0;
 // controlls what you see
 var scene = 1;
 var inGame = false;
@@ -43,31 +44,58 @@ button.prototype.hover = function (lll) {
   if (this.x < mouseX && this.y < mouseY && 
       this.x + this.width > mouseX && this.y + this.height > mouseY && 
       scene === this.scene) {
+		if (scene != 9 || (this.words === "Main Menu" || this.words === "back")) {
+			if (difficulty === 1) {
+				fill(102, 102, 0);
+			}
+			else if (difficulty === 2) {
+				fill(102, 51, 10);
+			}
+			else {
+				fill(102, 10, 10);
+			}
+			if (lll === 3) {
+				fill(102, 102, 0);
+			}
+			else if (lll === 2) {
+				fill(102, 51, 10);
+			}
+			else if (lll === 1) {
+				fill(102, 10, 10);
+			}
+			rect(this.x, this.y, this.width, this.height);
+			fill(255, 255, 255);
+			text(this.words, this.x + 5, this.y + 5, 
+				 this.width - 5, 
+				 this.height - 5);
+			
+		} else {
 		if (difficulty === 1) {
-    	fill(102, 102, 0);
+				fill(0, 102, 102);
+			}
+			else if (difficulty === 2) {
+				fill(10, 51, 102);
+			}
+			else {
+				fill(10, 10, 102);
+			}
+			if (lll === 3) {
+				fill(0, 102, 102);
+			}
+			else if (lll === 2) {
+				fill(10, 51, 102);
+			}
+			else if (lll === 1) {
+				fill(10, 10, 102);
+			}
+  		rect(this.x, this.y, this.width, this.height);
+    	fill(255, 255, 255);
+  		text(this.words, this.x + 5, this.y + 5, 
+      	 this.width - 5, 
+      	 this.height - 5);
 		}
-		else if (difficulty === 2) {
-			fill(102, 51, 10);
-		}
-		else {
-			fill(102, 10, 10);
-		}
-		if (lll === 3) {
-    	fill(102, 102, 0);
-		}
-		else if (lll === 2) {
-			fill(102, 51, 10);
-		}
-		else if (lll === 1) {
-			fill(102, 10, 10);
-		}
-  	rect(this.x, this.y, this.width, this.height);
-    fill(255, 255, 255);
-  	text(this.words, this.x + 5, this.y + 5, 
-       this.width - 5, 
-       this.height - 5);
-    this.hovering = true;
-  } 
+		this.hovering = true;
+	}
   else {
     if (scene === this.scene) {
 			if (difficulty === 1) {
@@ -109,6 +137,7 @@ var buttonMenu2 = new button (10, 10, 75, 25, 3, "Main Menu");
 var buttonMenu3 = new button (10, 10, 75, 25, 5, "Main Menu");
 var buttonMenu4 = new button (10, 10, 75, 25, 6, "Main Menu");
 var buttonMenu5 = new button (315, 365, 75, 25, 7, "Main Menu");
+var buttonMenu6 = new button (10, 10, 75, 25, 9, "Main Menu");
 var buttonStart1 = new button (10, 50, 120, 40, 1, "Start");
 var buttonSettings1 = new button (10, 100, 120, 40, 1, "Settings");
 var buttonDifficulty1 = new button (20, 50, 75, 30, 2, "Difficulty");
@@ -120,6 +149,9 @@ var Back1 = new button (325, 10, 50, 20, 2, "back");
 var Back2 = new button (325, 10, 50, 20, 3, "back");
 var Back3 = new button (325, 10, 50, 20, 5, "back");
 var Back4 = new button (325, 10, 50, 20, 6, "back");
+var Back5 = new button (325, 10, 50, 20, 9, "back");
+var buttonNumOfPlayer1 = new button (0, 0, 400, 200, 9, "One Player");
+var buttonNumOfPlayer2 = new button (0, 200, 400, 200, 9, "Two Players");
 var keybind = new button (20, 100, 75, 30, 2, "Controls");
 var next1 = new button (325, 355, 50, 20, 5, "Next");
 var prev1 = new button (10, 355, 50, 20, 6, "Prev");
@@ -227,6 +259,8 @@ function draw() {
     buttonStart1.hover();
     buttonSettings1.hover();
 		credits1.hover();
+		buttonNumOfPlayer1.hover();
+		buttonNumOfPlayer2.hover();
     textSize(16);
     buttonDifficulty1.hover();
     buttonDifficulty2.hover();
@@ -234,6 +268,9 @@ function draw() {
 		HardCpu.hover(1);
 		MediumCpu.hover(2);
 		EasyCpu.hover(3);
+		textSize(12);
+		buttonMenu6.hover();
+		Back5.hover();
 		if (scene === 7) {
 			background(155, 0, 155);
 			fill(0, 0, 0);
@@ -273,80 +310,94 @@ function draw() {
 }
 function mousePressed () {
 	if (!inGame) {
-  	if (buttonMenu1.hovering || buttonMenu2.hovering) {
+  	if (buttonMenu1.hovering || buttonMenu2.hovering || buttonMenu6.hovering) {
 			scene = 1;
 		}
-		if (buttonMenu3.hovering || buttonMenu4.hovering || buttonMenu5.hovering) {    
+		else if (buttonMenu3.hovering || buttonMenu4.hovering || buttonMenu5.hovering) {    
 			scene = 1;
   	}
-    if (buttonSettings1.hovering) {
+		else if (Back5.hovering) {
+			scene = 1;
+		}
+    else if (buttonSettings1.hovering) {
       scene = 2;
     }
-    if (buttonDifficulty1.hovering || buttonDifficulty2.hovering) {
+    else if (buttonDifficulty1.hovering || buttonDifficulty2.hovering) {
       scene = 3;
     }
-		if (HardCpu.hovering) {
+		else if (HardCpu.hovering) {
 			scene = 2;
 			difficulty = 3;
 		}
-		if (MediumCpu.hovering) {
+		else if (MediumCpu.hovering) {
 			scene = 2;
 			difficulty = 2;
 		}
-		if (EasyCpu.hovering) {
+		else if (EasyCpu.hovering) {
 			scene = 2;
 			difficulty = 1;
 		}
-		if (Back1.hovering) {
+		else if (Back1.hovering) {
 			scene = 1;
 		}
-		if (Back2.hovering) {
+		else if (Back2.hovering) {
 			scene = 2;
 		}
-		if (Back3.hovering || Back4.hovering) {
+		else if (Back3.hovering || Back4.hovering) {
 			scene = 2;
 		}
-		if (keybind.hovering) {
+		else if (keybind.hovering) {
 			scene = 5;
 		}
-		if (P1Up.hovering) {
+		else if (P1Up.hovering) {
 			changeKey = 1;
 		}
-		if (P1Down.hovering) {
+		else if (P1Down.hovering) {
 			changeKey = 2;
 		}
-		if (P1Left.hovering) {
+		else if (P1Left.hovering) {
 			changeKey = 3;
 		}
-		if (P1Right.hovering) {
+		else if (P1Right.hovering) {
 			changeKey = 4;
 		}
-		if (P2Up.hovering) {
+		else if (P2Up.hovering) {
 			changeKey = 5;
 		}
-		if (P2Down.hovering) {
+		else if (P2Down.hovering) {
 			changeKey = 6;
 		}
-		if (P2Left.hovering) {
+		else if (P2Left.hovering) {
 			changeKey = 7;
 		}
-		if (P2Right.hovering) {
+		else if (P2Right.hovering) {
 			changeKey = 8;
 		}
-		if (P1Shoot.hovering) {
+		else if (P1Shoot.hovering) {
 			changeKey = 9;
 		}
-		if (P2Shoot.hovering) {
+		else if (P2Shoot.hovering) {
 			changeKey = 10;
 		}
-		if (next1.hovering) {
+		else if (next1.hovering) {
 			scene = 6;
 		}
-		if (prev1.hovering) {
+		else if (prev1.hovering) {
 			scene = 5;
 		}
-		if (credits1.hovering) {
+		else if (credits1.hovering) {
 			scene = 7;
+		}
+		else if (buttonStart1.hovering) {
+			scene = 9;
+		}
+		else if (buttonNumOfPlayer1.hovering) {
+			Players = 1;
+			scene = 10;
+		}
+		else if (buttonNumOfPlayer2.hovering) {
+			Players = 2;
+			scene = 10;
 		}
   }
 }
