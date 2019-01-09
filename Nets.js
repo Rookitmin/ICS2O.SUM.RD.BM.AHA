62602902e1130ed06f7fcc62339ef80726b209a9
Rookitmin <42980035+Rookitmin@users.noreply.github.com>
2019-01-09 15:16:20 -0500

    Update Nets.js

--- Nets.js
+++ Nets.js
@@ -3,29 +3,20 @@ function Net (x1, y1, x2, y2, facing) {
 	this.p2 = createVector(x2, y2);
 	this.facing = facing;
 	this.testPuck = function (pX, pY, pD, team) {
-		if (pX > this.p1.x && pX < this.p2.x && ((pY >= this.p1.y && this.facing === 1 && pD > 90 && pD < 270) || (pY <= this.p1.y && this.facing === -1))) {
+		if (pX > this.p1.x && pX < this.p2.x && 
+				((pX >= this.p1.x && this.facing === 1 && pD > 180 && pD < 360) || 
+				 (pX <= this.p1.X && this.facing === -1 && pD < 180 && pD > 0))) {
+			console.log(16);
 			if (team === 1) {
-				score.t1 ++;
+				Score ++;
+				puck1 = [0, 0, 0, 0];
+				console.log(15);
 			}
 			else {
-				score.t2 ++;
+				Score2 ++;
+				puck1 = [0, 0, 0, 0];
+				console.log(17);
 			}
 		}
-	}
-	
-	this.draw = function () {
-		stroke(0, 0, 0);
-		strokeWeight(5);
-		if (this.facing === 0) {
-			line(this.p1.x, this.p1.y, this.p1.x, this.p2.y);
-			line(this.p1.x, this.p2.y, this.p2.x, this.p2.y);
-			line(this.p2.x, this.p2.y, this.p2.x, this.p1.y);
-		}
-		else {
-			line(this.p1.x, this.p1.y, this.p1.x, this.p2.y);
-			line(this.p1.x, this.p1.y, this.p2.x, this.p1.y);
-			line(this.p2.x, this.p2.y, this.p2.x, this.p1.y);
-		}
-		strokeWeight(1);
 	}
 }
