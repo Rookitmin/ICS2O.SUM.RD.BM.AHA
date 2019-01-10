be10c34a389442b7c71128b30f5256abb6ac1f2f
Rookitmin <42980035+Rookitmin@users.noreply.github.com>
2019-01-10 13:52:57 -0500

    Update Nets.js

--- Nets.js
+++ Nets.js
@@ -2,19 +2,21 @@ function Net (x1, y1, x2, y2, facing) {
 	this.p1 = createVector(x1, y1);
 	this.p2 = createVector(x2, y2);
 	this.facing = facing;
+	
 	this.testPuck = function (pX, pY, pD, team) {
-		if (pX > this.p1.x && pX < this.p2.x && 
-				((pX >= this.p1.x && this.facing === 1 && pD > 180 && pD < 360) || 
-				 (pX <= this.p1.X && this.facing === -1 && pD < 180 && pD > 0))) {
+		console.log("1" + (pX <= this.p1.X));
+		if (pY > this.p1.y && pY < this.p2.y && 
+				((pX <= this.p1.x && this.facing === 1 && pD > 180 && pD < 360) || 
+				 (pX >= this.p1.X && this.facing === -1 && pD < 180 && pD > 0))) {
 			console.log(16);
 			if (team === 1) {
 				Score ++;
-				puck1 = [0, 0, 0, 0];
+				puck1 = [400, 200, 0, 0];
 				console.log(15);
 			}
 			else {
 				Score2 ++;
-				puck1 = [0, 0, 0, 0];
+				puck1 = [400, 200, 0, 0];
 				console.log(17);
 			}
 		}
